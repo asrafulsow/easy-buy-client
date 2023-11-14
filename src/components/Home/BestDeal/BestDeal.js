eb49c6349b1fa9b944b142fa4edd1702d25f4c38
sujonphero <sujonsheikh.in@gmail.com>
2023-11-14 20:44:18 +0600

    done home page deals section

--- src/components/Home/BestDeal/BestDeal.js
+++ src/components/Home/BestDeal/BestDeal.js
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from 'next/image';
 import React from 'react';
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
@@ -5,9 +7,31 @@ import watch from "../../../assets/images/products/watch-9.png"
 
 
 const BestDeal = () => {
+
+    const products = [
+        {
+            img: watch,
+            des: "Smart Watch",
+            title: "Sony lady Fitness Watch",
+            price: "$70"
+        },
+        {
+            img: watch,
+            des: "Smart Watch",
+            title: "Sony lady Fitness Watch",
+            price: "$70"
+        },
+        {
+            img: watch,
+            des: "Smart Watch",
+            title: "Sony lady Fitness Watch",
+            price: "$70"
+        }
+    ]
+
     return (
         <div className='bg-[#e3ebf1]'>
-            <div className='mx-[150px]'>
+            <div className='md:mx-[120px] py-20'>
                 {/* TOP TITLE */}
                 <div className='flex justify-between items-center py-8'>
                     <h1 className='text-3xl font-bold'>
@@ -21,18 +45,29 @@ const BestDeal = () => {
                 </div>
 
                 <div>
-                    <div>
-                        <Image
-                            src={watch}
-                            width={500}
-                            height={500}
-                            alt='product img'
-                        />
-                        <hr />
+                    <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                        {
+                            products.map((product) => (
+                                <div className='bg-white rounded-md p-10'>
+                                    <Image
+                                        src={product.img}
+                                        width={300}
+                                        height={300}
+                                        alt='product img'
+                                        className='hover:scale-110 duration-300 cursor-pointer'
+                                    />
+                                    <hr />
+                                    <div className='space-y-2'>
+                                        <p className='text-slate-500 hover:text-blue-500 duration-300 cursor-pointer'>{product.des}</p>
 
-                        <p>Smart Watch</p>
-                        <h3>Sony lady fitness watch.</h3>
+                                        <h1 className='text-lg font-semibold hover:text-blue-500 duration-300 cursor-pointer'>{product.title}</h1>
 
+                                        <p className='text-blue-600 font-medium'>{product.price}</p>
+                                    </div>
+                                </div>
+
+                            ))
+                        }
                     </div>
                 </div>
             </div>
