66937e3f88185cacdc03edda44ef9b6629f66c46
sujonphero <sujonsheikh.in@gmail.com>
2023-11-14 21:27:13 +0600

    done gallery section

--- src/components/Home/Gallery/Gallery.js
+++ src/components/Home/Gallery/Gallery.js
@@ -1,9 +1,48 @@
+"use client"
+
+import Image from 'next/image';
 import React from 'react';
+import art from "../../../assets/images/random/flower.jpg"
+
 
 const Gallery = () => {
+    const gallerys = [
+        {
+            img: art,
+        },
+        {
+            img: art,
+        },
+        {
+            img: art,
+        },
+        {
+            img: art,
+        }
+    ]
+
     return (
-        <div>
-            <h1>This is Gallery area</h1>
+        <div className='bg-[#e3ebf1]'>
+            <div className='md:mx-[120px] py-20'>
+                <div>
+                    <div className=' grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-2'>
+                        {
+                            gallerys.map((gallery) => (
+                                <div className='rounded-md'>
+                                    <Image
+                                        src={gallery.img}
+                                        width={1000}
+                                        height={1000}
+                                        alt='product img'
+                                        className='rounded-md hover:scale-105 duration-500 cursor-pointer'
+                                    /> <br />
+                                </div>
+
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
